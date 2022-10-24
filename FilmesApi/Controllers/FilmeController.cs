bf795071ee9b102471d986904ccca5187d1c5fea
Joshua Abbate <55669412+joshuajka@users.noreply.github.com>
2022-10-24 10:06:53 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void adicionaFIlme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
